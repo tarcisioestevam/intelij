d28461e216a957caa8e22feae143995f274fb401
tarcisioestevam <tarcisioestevam@gmail.com>
2023-03-13 00:24:28 -0300

    First Commit

---
+++ src/br/com/br/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.br;
+
+import br.com.br.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
